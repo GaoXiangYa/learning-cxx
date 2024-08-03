48dd7d17426babbb6599e3bce6b56e4c2ed3b2a4
GaoXiangYa <gx19991105@outlook.com>
2024-08-03 18:37:21 +0800

    rewrite 1~8

--- exercises/07_enum&union/main.cpp
+++ exercises/07_enum&union/main.cpp
@@ -37,7 +37,8 @@ ColorEnum convert_by_pun(Color c) {
 
     TypePun pun;
     // TODO: 补全类型双关转换
-
+    int tmp = static_cast<int>(c);
+    pun.e = static_cast<ColorEnum>(tmp);
     return pun.e;
 }
 
